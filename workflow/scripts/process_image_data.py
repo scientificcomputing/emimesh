ef18d414cb4f1dab7a5ef58b0d91bd59bda3dc70
Marius Causemann <mariusca@simula.no>
2023-12-19 11:38:57 +0100

    use fastmproh

--- workflow/scripts/process_image_data.py
+++ workflow/scripts/process_image_data.py
@@ -1,8 +1,4 @@
-from dask_image.ndmorph import binary_opening, binary_closing, binary_erosion, binary_dilation
-import dask.array as da
 import numpy as np
-from skimage.segmentation import expand_labels
-import skimage.morphology as skim
 import argparse
 import yaml
 import json
@@ -10,84 +6,33 @@ import time
 import pyvista as pv
 from pathlib import Path
 from utils import get_cell_frequencies, np2pv
-import dask
 from dask_image.ndinterp import affine_transform
 import fastremap
-
-def binary_smoothing(img, radius, dx, iter=1):
-    ball = skim.ball(int(radius / dx))
-    for i in range(iter):
-        img = binary_opening(img, structure=ball, iterations=iter)
-        img = binary_closing(img, structure=ball, iterations=iter)
-    return img
+import cc3d
+import fastmorph
+import skimage.morphology as skim
 
 
-def label_smoothing(img, label, radius, dx, background_value=0, iter=1):
-    mask = da.isin(img, label)
-    img[mask] = background_value
-    mask = binary_smoothing(mask, iter=iter, radius=radius, dx=dx)
-    img[mask] = label
-    return img
-
-
-def label_erosion(img, label, radius, dx, background_value=0):
-    mask = da.isin(img, label)
-    ball = skim.ball(int(radius / dx))
-    img[mask] = background_value
-    mask = binary_erosion(mask, structure=ball)
-    img[mask] = label
-    return img
-
-
-def simplify_img(
-    img, resolution, cois, expand_voxel_dist, smoothing_iter, smoothing_radius, shrink
-):
-    dx = max(resolution)
-    start = time.time()
-    img[np.isin(img, cois) == False] = 0
-    if expand_voxel_dist > 0:
-        img = expand_labels(img, distance=expand_voxel_dist)
-    expand_time = time.time()
-    print(f"expansion time: {expand_time - start} s")
-
-    expand_time = time.time()
-    img_da = da.from_array(img)
-    print((img_da > 0).sum().compute())
-    if smoothing_iter > 0:
-        for cid in cois:
-            img_da = label_smoothing(
-                img_da, cid, iter=smoothing_iter, radius=smoothing_radius, dx=dx
-            )
-    img_da.compute()
-    smooth_time = time.time()
-
-    print(f"smooth time: { smooth_time - expand_time} s")
-
-    for cid in cois:
-        img_da = label_erosion(
-            img_da, cid, background_value=0, radius=dx * shrink, dx=dx
-        )
-    img_da.compute()
-    erosion_time = time.time()
-
-    print(f"erosion time: { erosion_time - smooth_time} s")
-    return img_da
-
-def set_boundary_value(img, value, extent=1):
-    img[0:extent,:,:] = value
-    img[-extent:,:,:] = value
-    img[:, 0:extent,:] = value
-    img[:, -extent:,:] = value
-    img[:,:, 0:extent] = value
-    img[:,:, -extent:] = value
-
-
-def get_roi_mask(img, roi_cells, dilate=5, iterations=4):
-    roi_mask = da.isin(img, roi_cells)
-    ball = skim.ball(dilate)
-    roi_mask = binary_dilation(roi_mask, ball, iterations=iterations)
+def get_roi_mask(img, roi_cells, radius, iterations, res):
+    roi_mask = fastremap.mask_except(img, roi_cells) > 0
+    for i in range(iterations):
+        roi_mask = fastmorph.spherical_dilate(roi_mask, radius=radius*res[0], parallel=4, anisotropy=res)
     return roi_mask
 
+def remap_labels(img, cells=None):
+    img, remapping = fastremap.renumber(img, in_place=True)
+    if cells is not None:
+        startid = max(remapping.values()) + 1
+        fastremap.refit(img, startid + len(cells))
+        new_ids = [remapping[cid] for cid in cells]
+        remap_dict = {new_ids[i]:startid + i for i in range(len(cells))}
+        img = fastremap.remap(img, remap_dict, preserve_missing_labels=True, in_place=True)
+        remapping.update({cells[i]:startid + i for i in range(len(cells))})
+    return img, remapping
+
+def merge_labels(img, labels):
+    print(f"merging cells: {labels}")
+    return fastremap.remap(img, {l:labels[0] for l in labels}, in_place=True, preserve_missing_labels=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -108,51 +53,52 @@ if __name__ == "__main__":
     parser.add_argument(
         "--output", help="output filename", type=str, default="processeddata.xdmf"
     )
-    parser.add_argument("--roi", nargs='*', 
+    parser.add_argument("--roi", nargs='*',  type=int,
                         help="specify region of interest by cell id")
     parser.add_argument("--roidilate", help="dilate roi", type=str, default="0-0")
-    parser.add_argument("--cells", nargs='*', 
+    parser.add_argument("--cells", nargs='*', type=int,
                         help="specify cell ids to be included")
-    parser.add_argument('--merge', type=str)
+    parser.add_argument('--merge', nargs='*', type=int,)
     parser.add_argument("--nworkers", help="number of threads", type=int)
     parser.add_argument("--dx", help="target resolution", type=int, default=None)
 
     args = parser.parse_args()
-    dask.config.set(scheduler="threads", num_workers=args.nworkers)
-    print(f"Using {args.nworkers} workers...")
+    parallel = args.nworkers
+    print(f"Using {parallel} workers...")
 
     start = time.time()
     imggrid = pv.read(args.infile)
+    img = imggrid["data"]
     dims = imggrid.dimensions
     resolution = imggrid.spacing
-    img, remapping = fastremap.renumber(imggrid["data"], in_place=True)
+
+    img, remapping = remap_labels(img, args.cells)
+
+    if args.merge:
+        img = merge_labels(img, [remapping[l] for l in args.merge])
+
     img = img.reshape(dims - np.array([1, 1, 1]), order="F")
     
     dx = max(resolution)
     if args.dx:
         dx = args.dx
-    scale = np.diag([dx / r for r in resolution] + [1])
+        scale = np.diag([dx / r for r in resolution] + [1])
 
-    new_dims = [int(d * r / dx) for d,r in zip(dims, resolution)]
-    isotropic_img = affine_transform(img, scale,
-                                     output_shape=new_dims,
-                                     order=0)
-    img = np.array(isotropic_img)
+        new_dims = [np.floor(d * r / dx) for d,r in zip(dims, resolution)]
+        isotropic_img = affine_transform(img, scale,
+                                        output_shape=new_dims,
+                                        order=0)
+        img = np.array(isotropic_img)
+        resolution = [dx]*3
+    imggrid = np2pv(img, [dx]*3)
+    imggrid["resampled"] = img.flatten(order="F")
     
-    if args.merge is not None:
-        cells_to_merge = [remapping[int(cid)] for cid in args.merge.split("-")]
-        print(f"merging cells: {cells_to_merge}")
-        img[np.isin(img, cells_to_merge)] = cells_to_merge[0]
-
     if args.roi is not None:
-        roi_cells = [remapping[int(cid)] for cid in args.roi]
-        print(f"using mask of the following cells: {roi_cells}")
+        roi_cells = [remapping[int(l)] for l in args.roi]
         roidilate, rioiter = [int(i) for i in args.roidilate.split("-")]
-        roimask = get_roi_mask(img, roi_cells, dilate=roidilate, iterations=rioiter)
-        img[roimask==0] = 0
-        set_boundary_value(roimask, 0, extent=5)
-        roimask = binary_erosion(roimask, skim.ball(1))
-        assert roimask.sum() > 0
+        roimask = get_roi_mask(img, roi_cells, radius=roidilate, iterations=rioiter, res=resolution)
+        extended_roi = fastmorph.spherical_dilate(roimask, radius=100, parallel=parallel, anisotropy=resolution)
+        img[extended_roi==0] = 0
     else:
         roimask = None
 
@@ -160,6 +106,7 @@ if __name__ == "__main__":
     print(f"load data time: {load - start} s")
     cell_labels, cell_counts = get_cell_frequencies(img)
     print(cell_labels)
+
     freq_time = time.time()
     print(f"get cell frequency time: {freq_time - load} s")
 
@@ -170,33 +117,57 @@ if __name__ == "__main__":
         original_cell_counts=cell_counts,
     )
     if args.ncells < len(cell_labels):
-        #if args.cells is not None:
         cois = cell_labels[-args.ncells :]
     else:
         cois = cell_labels
 
-    da_img = simplify_img(
-        img,
-        resolution,
-        cois,
-        args.expand,
-        args.smoothiter,
-        args.smoothradius,
-        args.shrink,
-    )
+
+    print("start processing..")
+
+    img = fastremap.mask_except(img, list(cois))
+    imggrid["masked"] = img.flatten(order="F")
+
+    cc3d.dust(img, threshold=100, connectivity=6, in_place=True)
+    img = fastmorph.fill_holes(img, remove_enclosed=False)
+    from IPython import embed; embed()
+    
+    for i in range(args.expand):
+        img = fastmorph.dilate(img, background_only=False, parallel=parallel)
+    #img = expand_labels(img, distance=args.expand)
+    imggrid["dilated"] = img.flatten(order="F")
+
+    for i in range(args.smoothiter):
+        img = skim.opening(img, footprint=skim.ball(4))
+        img = skim.closing(img, footprint=skim.ball(4))
+        #img = fastmorph.opening(img, background_only=False,parallel=parallel)
+        #img = fastmorph.closing(img, parallel=parallel)
+
+    imggrid["smoothed"] = img.flatten(order="F")
+
+    for i in range(args.shrink):
+        img = fastmorph.erode(img, parallel=parallel)
+
+    imggrid["eroded"] = img.flatten(order="F")
+
+    img[roimask==0] = 0
+    cc3d.dust(img, threshold=100, connectivity=6, in_place=True)
+    imggrid["data"] = img.flatten(order="F")
+
+    for i in range(3):
+        roimask = fastmorph.erode(roimask,parallel=parallel)
+    imggrid["roimask"] = roimask.flatten(order="F")
+
     proc_time = time.time()
     print(f"total processing time: {proc_time - freq_time} s")
-    img = np.array(da_img)
-    #island_threshold = 64
-    #img = skim.remove_small_objects(img, island_threshold)
 
-    imggrid = np2pv(img, [dx]*3,
-                     roimask=np.array(roimask) if roimask is not None else None)
+    resdir = Path(args.output).parent
+
+    resdir.mkdir(parents=True, exist_ok=True)
     imggrid.save(args.output)
     save_time = time.time()
     print(f"saving time: {save_time - proc_time} s")
 
-    cell_labels, cell_counts = get_cell_frequencies(imggrid["data"])
+    cell_labels, cell_counts = get_cell_frequencies(img)
     mesh_statistics["cell_labels"] = cell_labels
     mesh_statistics["cell_counts"] = cell_counts
     mesh_statistics["mapping"] = remapping
@@ -204,5 +175,5 @@ if __name__ == "__main__":
     for k, v in mesh_statistics.items():
         mesh_statistics[k] = np.array(v).tolist()
 
-    with open(Path(args.output).parent / "imagestatistic.yml", "w") as mesh_stat_file:
+    with open(resdir / "imagestatistic.yml", "w") as mesh_stat_file:
         yaml.dump(mesh_statistics, mesh_stat_file)
