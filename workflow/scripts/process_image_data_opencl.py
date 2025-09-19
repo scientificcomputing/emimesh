008a47fd66875dd5cd4e13d0b5b3347ff0aa37a2
Marius Causemann <mariuscausemann@yahoo.de>
2025-09-19 18:32:46 +0200

    replace pyclesperanto with nbmorph

--- workflow/scripts/process_image_data_opencl.py
+++ workflow/scripts/process_image_data_opencl.py
@@ -5,18 +5,19 @@ import json
 import time
 import pyvista as pv
 from pathlib import Path
-from utils import np2pv
+from utils import get_cell_frequencies, np2pv
 from dask_image.ndinterp import affine_transform
 import fastremap
+import skimage.morphology as skim
+import pyclesperanto_prototype as cle
+from cle_patch import opening_labels, closing_labels, erode_labels
 import dask.array as da
 import dask
 from functools import partial
+from collections.abc import Iterable
 import cc3d
-import numba
-import nbmorph
-
-numba.set_num_threads(1)
-dask.config.set({"array.chunk-size": "1024 MiB"})
+cle.set_wait_for_kernel_finish(True)
+dask.config.set({"array.chunk-size": "512 MiB"})
 
 def mergecells(img, labels):
     print(f"merging cells: {labels},  ({img.shape})")
@@ -27,26 +28,30 @@ def ncells(img, ncells):
     cell_labels, cell_counts = fastremap.unique(img, return_counts=True)
     cell_labels = cell_labels[np.argsort(cell_counts)]
     cois = list(cell_labels[-ncells :])
-    img = np.where(np.isin(img, cois), img, 0)
+    img = da.where(da.isin(img, cois), img, 0)
     return img
     
-def dilate(img, radius, labels=None):
+def dilate(img, iterations, radius, labels=None):
     print(f"dilating cells,  ({img.shape})")
     if labels is None:
-        img = nbmorph.dilate_labels_spherical(img, radius=radius)
+        for i in range(iterations):
+            img = cle.dilate_labels(img, radius=radius)
+        img = cle.pull(img)
     else:
         vipimg = np.where(np.isin(img, labels), img, 0)
-        vipimg = dilate(vipimg, radius=radius)
+        vipimg = dilate(vipimg, iterations=iterations, radius=radius)
         img = np.where(vipimg, vipimg, img)
     return img
 
-def erode(img, radius, labels=None):
+def erode(img, iterations, radius, labels=None):
     print(f"eroding cells,  ({img.shape})")
     if labels is None:
-        img = nbmorph.erode_labels_spherical(img, radius=radius)
+        for i in range(iterations):
+            img = erode_labels(img, radius=radius)
+        img = cle.pull(img)
     else:
         vipimg = np.where(np.isin(img, labels), img, 0)
-        vipimg = erode(vipimg, radius=radius)
+        vipimg = erode(vipimg, iterations=iterations, radius=radius)
         orig_wo_vips = np.where(np.isin(img, labels), 0, img)
         img = np.where(orig_wo_vips > vipimg, orig_wo_vips, vipimg)
     return img
@@ -54,8 +59,10 @@ def erode(img, radius, labels=None):
 def smooth(img, iterations, radius, labels=None):
     print(f"smoothing cells,  ({img.shape})")
     if labels is None:
-        img = nbmorph.smooth_labels_spherical(img, radius=radius,
-                                              iterations=iterations, dilate_radius=radius)
+        for i in range(iterations):
+            img = opening_labels(img, radius=radius)
+            img = closing_labels(img, radius=radius)
+        img = cle.pull(img)
     else:
         vipimg = np.where(np.isin(img, labels), img, 0)
         vipimg = smooth(vipimg, iterations=iterations, radius=radius)
@@ -68,8 +75,12 @@ def smooth(img, iterations, radius, labels=None):
 def removeislands(img, minsize):
     return cc3d.dust(img, threshold=minsize, connectivity=6)
 
+    #would be more performant, but relabels all cells..
+    #return cle.exclude_small_labels(img, maximum_size=minsize)
+
 opdict ={"merge": mergecells, "smooth":smooth, "dilate":dilate,
          "erode":erode, "removeislands":removeislands, "ncells":ncells}
+
 
 def _parse_to_dict(values):
     result = {}
@@ -98,19 +109,22 @@ if __name__ == "__main__":
     parser.add_argument('-o','--operation', nargs='+', action='append', help="operations to be performed on the segmented image")
 
     args = parser.parse_args()
-    n_parallel = args.nworkers
-    print(f"Using {n_parallel} workers...")
-    start = time.time()
+    parallel = args.nworkers
+    print(f"Using {parallel} workers...")
+    print(f"Available  devices {cle.available_device_names()}")
+    cle.select_device("NVIDIA")
+    dev = cle.get_device()
+    print(f"Running processing on {dev}")
+    mem_gb = dev.device.global_mem_size / 1e9
+    print(f"device memory detected: {mem_gb:.1f} GB")
 
-    # read image file
+    start = time.time()
     imggrid = pv.read(args.infile)
     img = imggrid["data"]
     dims = imggrid.dimensions
     resolution = imggrid.spacing
     img = img.reshape(dims - np.array([1, 1, 1]), order="F")
     img = da.from_array(img)
-
-    # get cells labels, and filter by n-largest (if requested)
     cell_labels, cell_counts = fastremap.unique(img, return_counts=True)
     if args.ncells:
         cell_labels = list(cell_labels[np.argsort(cell_counts)])
@@ -121,27 +135,22 @@ if __name__ == "__main__":
         cell_labels = list(cell_labels)
         cell_labels.remove(0)
 
-    # remap labels to smaller, sequential ints
     remapping = {int(c):i for i,c in enumerate([0] + cell_labels)}
     remap = lambda ids: [remapping[int(i)] for i in ids if i in remapping.keys()]
     img = img.map_blocks(partial(fastremap.remap, table=remapping), dtype=img.dtype)
     img = img.map_blocks(partial(fastremap.refit, value=len(cell_labels)))
 
-    # interpolate into the specified, isotropic grid with size dx
     dx = args.dx
     scale = np.diag([dx / r for r in resolution] + [1])
     new_dims = [int(d * r / dx) for d,r in zip(dims, resolution)]
     img = affine_transform(img, scale, output_shape=new_dims, order=0,
                            output_chunks=500)
-    img = dask.compute(img, num_workers= args.nworkers)[0]
     print(f"image size: {img.shape}")
     resolution = [dx]*3
     roi = None
-
-    # parse user specified operations, and iterate over them:
     operations = parse_operations(args.operation)
+
     for op, kwargs in operations:
-        print(op, kwargs)
         if "labels" in kwargs.keys():
             labels = kwargs["labels"]
             if kwargs.get("allexcept", False):
@@ -150,25 +159,28 @@ if __name__ == "__main__":
                 kwargs["labels"] = remap(labels)
 
         if op=="roigenerate":
-            roi = np.isin(img, kwargs["labels"])
+            roi = da.isin(img, kwargs["labels"])
             continue
         if op=="roiapply":
-            img = np.where(roi, img, 0) 
+            img = da.where(roi, img, 0) 
             continue
         if op=="ncells":
             img = ncells(img, **kwargs)
             continue
         if op.startswith("roi"):
             roiop = op[3:]
-            roi = opdict[roiop](roi, **kwargs)
+            roi = roi.map_overlap(partial(opdict[roiop], **kwargs), depth=30, dtype=np.uint8)
         else:
-            img =opdict[op](img, **kwargs)
+            img = img.map_overlap(partial(opdict[op], **kwargs), depth=30, dtype=img.dtype)
     
-    #img, roi = dask.compute(img, roi, num_workers= args.nworkers)
+    chunk_mem_gb = np.prod(img.chunksize) * np.nbytes[img.dtype] / 1e9
+    print(f"chunk size: {chunk_mem_gb} GB")
+    max_workers = int(mem_gb / (chunk_mem_gb*3)) # assume max 3x chunk memory is used
+    img, roi = dask.compute(img, roi, num_workers=min(max_workers, args.nworkers))
     print(f"processed! {img.shape}")
     proc_time = time.time()
 
-    # remap labels to smaller, sequential ints again, since many labels might have disappeared...
+    img = da.array(img)
     cell_labels, cell_counts = fastremap.unique(img, return_counts=True)
     cell_labels = list(cell_labels[np.argsort(cell_counts)])
     cell_labels.remove(0)
@@ -178,7 +190,7 @@ if __name__ == "__main__":
     remapping2.update({int(c):i + 2 for i,c in enumerate(cell_labels)})
     img = img.map_blocks(partial(fastremap.remap, table=remapping2), dtype=img.dtype)
     img = img.map_blocks(partial(fastremap.refit, value=max(remapping2.values())))
-    img = dask.compute(img, num_workers= args.nworkers)[0]
+    img = dask.compute(img, num_workers=min(max_workers, args.nworkers))[0]
 
     imggrid = np2pv(img, resolution)
     if roi is not None:
